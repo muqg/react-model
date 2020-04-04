7b36ab828e4d75052a4185f537b3fdc321c578f2
Lubo <muqglu@gmail.com>
2020-04-04 13:41:16 +0300

    Add useModel property and method selectors
    This adds the ability to call useModel with the string name of a model's property or method and receive it as the result of the hook call. This subscribes the component to changes to the selected property. Does not subscribe to updates when selecting a method since methods never change and is unnecessary to subscribe.
    
    ---
    
    This is a way to opt-in for optimal performance when it is necessary to access a model property or method in a component that is heavy to re-render on every single model change.
    
    For example when we would like to use the model submission method directly inside our form, which renders all of the input fields, we would call useModel("submit"). This way when any of the input changes and updates the model, the entire form will not be rerendered, since the submit method has not changed. Only the changed input component would render.
    
    Similarly if we call useModel("isDirty") the calling component will only update when the isDirty value changes.
    
    In contrast if we were to call useModel() and then access model.isDirty, the component will update on every single model change since useModel() subscribes to every single model change.

--- src/__tests__/useModel.test.tsx
+++ src/__tests__/useModel.test.tsx
@@ -1,7 +1,11 @@
+import {fireEvent} from "@testing-library/react"
 import {renderHook} from "@testing-library/react-hooks"
 import React, {PropsWithChildren} from "react"
+import {Model} from "../Model"
 import {ModelProvider} from "../ModelProvider"
 import {useModel} from "../useModel"
+import {renderStrict} from "../util/TestingUtils"
+import {useField} from "../useField"
 
 type ProviderModelObject = {foo: string}
 
@@ -19,7 +23,7 @@ describe("Model hook", () => {
     const {message} = hook.result.error
 
     expect(message).toBe(
-      "useModel hook can only be called with no arguments in children of " +
+      "useModel hook can only be called with no arguments or a selector in children of " +
         "<ModelProvider />, otherwise you should provide it with an object schema."
     )
   })
@@ -52,5 +56,171 @@ describe("Model hook", () => {
     const model = hook.result.current
 
     expect(model.getField("foo").value).toBe("schema")
+  })
+
+  describe("selectors", () => {
+    it("subscribes to changes when selecting a model property", () => {
+      let model!: Model<any>
+      let spy!: jest.SpyInstance<any>
+
+      const Component = () => {
+        model = useModel()
+        spy = jest.spyOn(model, "_subscribe")
+
+        useModel("isDirty")
+
+        return null
+      }
+
+      renderStrict(
+        <ModelProvider schema={{foo: {value: 1}}}>
+          <Component />
+        </ModelProvider>
+      )
+
+      // Should be called twice -- once for the useModel() call
+      // and once for the property selector call.
+      expect(spy).toHaveBeenCalledTimes(2)
+    })
+
+    it("does not subscribe to changes when selecting a model method", () => {
+      let model!: Model<any>
+      let spy!: jest.SpyInstance<any>
+
+      const Component = () => {
+        model = useModel()
+        spy = jest.spyOn(model, "_subscribe")
+
+        useModel("getField")
+
+        return null
+      }
+
+      renderStrict(
+        <ModelProvider schema={{foo: {value: 1}}}>
+          <Component />
+        </ModelProvider>
+      )
+
+      // Should be called only once for the useModel() call
+      // and not for the property selector call.
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+
+    it("returns the selected method or property", () => {
+      let model!: Model<any>
+      let validate!: any
+      let values!: any
+
+      const Component = () => {
+        model = useModel()
+        validate = useModel("validate")
+        values = useModel("values")
+
+        return null
+      }
+
+      renderStrict(
+        <ModelProvider schema={{foo: {value: 1}}}>
+          <Component />
+        </ModelProvider>
+      )
+
+      expect(validate).toBe(model.validate)
+      expect(values).toBe(model.values)
+    })
+
+    it("returns the updated model property result when it changes", async () => {
+      let result: any
+
+      const Input = () => {
+        const {setValue} = useField("foo")
+        return <input data-testid="input" name="foo" onChange={setValue} />
+      }
+
+      const Component = () => {
+        result = useModel("isDirty")
+        return <Input />
+      }
+
+      const {getByTestId} = renderStrict(
+        <ModelProvider schema={{foo: {value: "initial"}}}>
+          <Component />
+        </ModelProvider>
+      )
+
+      fireEvent.change(getByTestId("input"), {target: {value: "changed"}})
+      expect(result).toBeTruthy()
+
+      fireEvent.change(getByTestId("input"), {target: {value: "initial"}})
+      expect(result).toBeFalsy()
+    })
+
+    it("does not allow private methods or properties to be selected", () => {
+      let subscribe!: any
+
+      const Component = () => {
+        subscribe = useModel("_subscribe")
+        return null
+      }
+
+      renderStrict(
+        <ModelProvider schema={{foo: {value: 1}}}>
+          <Component />
+        </ModelProvider>
+      )
+
+      expect(subscribe).toBeUndefined()
+    })
+
+    it("returns undefined when selecting invalid method or property", () => {
+      let result!: any
+
+      const Component = () => {
+        // @ts-ignore
+        result = useModel("invalid_key")
+        return null
+      }
+
+      renderStrict(
+        <ModelProvider schema={{foo: {value: 1}}}>
+          <Component />
+        </ModelProvider>
+      )
+
+      expect(result).toBeUndefined()
+    })
+
+    it("only updates when selected property changes", () => {
+      const Input = () => {
+        const {setValue} = useField("foo")
+        return <input data-testid="input" name="foo" onChange={setValue} />
+      }
+
+      const IsDirtySelector = jest.fn(() => {
+        useModel("isDirty")
+        return null
+      }) as React.FunctionComponent
+
+      const IsSubmittingSelector = jest.fn(() => {
+        useModel("isSubmitting")
+        return null
+      }) as React.FunctionComponent
+
+      const {getByTestId} = renderStrict(
+        <ModelProvider schema={{foo: {value: 1}}}>
+          <Input />
+          <IsDirtySelector />
+          <IsSubmittingSelector />
+        </ModelProvider>
+      )
+
+      jest.clearAllMocks()
+
+      fireEvent.change(getByTestId("input"), {target: {value: "changed"}})
+
+      expect(IsDirtySelector).toHaveBeenCalled()
+      expect(IsSubmittingSelector).not.toHaveBeenCalled()
+    })
   })
 })
--- src/useModel.ts
+++ src/useModel.ts
@@ -1,13 +1,14 @@
-import {Model} from "./Model"
 import {useContext, useEffect, useMemo} from "react"
+import {Model} from "./Model"
 import {ModelContext} from "./ModelProvider"
 import {ModelOptions, ModelSchema} from "./types"
+import {isObject} from "./util/ObjectUtils"
 import {useForceUpdate} from "./util/useForceUpdate"
 
 function assertContextModel(maybeModel: any): asserts maybeModel {
   if (!maybeModel) {
     throw new Error(
-      "useModel hook can only be called with no arguments in children of " +
+      "useModel hook can only be called with no arguments or a selector in children of " +
         "<ModelProvider />, otherwise you should provide it with an object schema."
     )
   }
@@ -22,6 +23,15 @@ function assertContextModel(maybeModel: any): asserts maybeModel {
  */
 export function useModel<T extends object>(): Model<T>
 /**
+ * Access a model method or property and subscribe to changes to it.
+ *
+ * @param selector Name of the model method or property.
+ */
+export function useModel<
+  T extends object,
+  U extends keyof Model<T> = keyof Model<T>
+>(selector: U): Model<T>[U]
+/**
  * Create a model instance from schema.
  *
  * @param schema Model object schema.
@@ -32,26 +42,57 @@ export function useModel<T extends object = any>(
   options?: ModelOptions
 ): Model<T>
 
-export function useModel<T extends object = any>(
-  schema?: ModelSchema<T>,
-  options: Partial<ModelOptions<T>> = {}
-): Model<T> {
-  const maybeContextModel = useContext(ModelContext) as Model<T> | null
+export function useModel(
+  schemaOrSelector?: ModelSchema | string,
+  options: Partial<ModelOptions> = {}
+): Model | Model[keyof Model] {
+  const maybeContextModel = useContext(ModelContext) as Model | null
 
-  const model = useMemo<Model<T>>(() => {
-    if (schema) {
-      return new Model<T>(schema, options)
+  const model = useMemo<Model>(() => {
+    if (isObject<ModelSchema>(schemaOrSelector)) {
+      return new Model(schemaOrSelector, options)
     } else {
       assertContextModel(maybeContextModel)
       return maybeContextModel
     }
   }, [])
 
+  let result: any = model
+  if (typeof schemaOrSelector === "string") {
+    if (schemaOrSelector.indexOf("_") === -1) {
+      result = model[schemaOrSelector]
+    } else {
+      result = undefined
+    }
+  }
+
   // TODO: Consider using React's useMutableSource hook once it becomes
   // available in place of the subscription based implementation.
   // @see https://github.com/facebook/react/pull/18000
   const forceUpdate = useForceUpdate()
-  useEffect(() => model._subscribe(forceUpdate), [forceUpdate, model])
+  useEffect(() => {
+    // Since model methods are always the same it is unnecessary to subscribe
+    // at all. They do not need to be updated on model change.
+    if (typeof result === "function") {
+      return
+    }
 
-  return model
+    return model._subscribe((instance) => {
+      // By default it is assumed that the result is the model itself and
+      // we would like to update anytime it changes.
+      let shouldUpdate = true
+
+      // If the result is a selected model property we only ever want to
+      // update when it has actually changed.
+      if (typeof schemaOrSelector === "string") {
+        shouldUpdate = instance[schemaOrSelector] !== result
+      }
+
+      if (shouldUpdate) {
+        forceUpdate()
+      }
+    })
+  }, [result, model, schemaOrSelector, forceUpdate])
+
+  return result
 }
